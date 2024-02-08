b2916a3ff62559cb091cd5b2a5e59dd183ba983a
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2024-02-08 16:39:38 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
-      <h1 className="text-xl font-bold">Rad-Pair Transcript Processor</h1>
+      <h1 className="text-xl font-bold">RadPair Transcript Processor</h1>
     </header>
   );
 }
